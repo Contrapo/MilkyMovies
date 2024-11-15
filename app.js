671b7fdda4e6e50d52f8ef9f7e80515f7d6ecef9
Contrapo <felipecarnauba033@gmail.com>
2024-11-15 01:07:56 -0300

    Commit 5

--- app.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from '../Navigation/appNavigation';
-
-export default function App() {
-    return ( 
-        <AppNavigation />
-    );
-}
